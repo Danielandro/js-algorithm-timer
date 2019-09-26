7e2e134d16cd687330cdc3b6a6b9f4474b342c1e
Danielandro <mtme13@hotmail.com>
2019-09-26 13:54:56 +0100

    Times logged for Array.reverse

--- algoTimer.js
+++ algoTimer.js
@@ -1,17 +1,21 @@
-const NumberOfIncrements = 10;
-const ArrayIncrements = 50_000;
+// const NumberOfIncrements = 10;
+// const ArrayIncrements = 50000;
 
 function timer(arrSize, algorithm) {
-  // let testArr = setupArray(arrSize);
-
   for (let i = 10; i <= arrSize; i += 10) {
     let testArr = setupArray(i);
+    // time at start  
+    console.time(`Array Size: ${testArr.length}`);
+    // call method to test
+    testArr.algorithm;
+    // time when finished 
+    console.timeEnd(`Array Size: ${testArr.length}`);
   }
 }
 
 function setupArray(arrSize) {
   let arr = [];
-  for (let i = 0; i < arrSize; i++) {
+  for (let i = 1; i <= arrSize; i++) {
     arr.push(i);
   }
 
@@ -22,6 +26,9 @@ module.exports = {
   timer
 };
 
+var reverse = Array.prototype.reverse;
+
+timer(1000000, reverse);
 // setup the array
 // time it with the chosen algorithm
 // grab the results
