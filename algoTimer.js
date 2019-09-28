fad5ebe4a05b9fc031074b2db5447b0b26cc34ff
Danielandro <mtme13@hotmail.com>
2019-09-28 14:11:37 +0100

    Timer works using Node Performance module

--- algoTimer.js
+++ algoTimer.js
@@ -1,15 +1,29 @@
 // const NumberOfIncrements = 10;
 // const ArrayIncrements = 50000;
+const {
+  performance,
+  PerformanceObserver
+} = require('perf_hooks');
 
 function timer(arrSize, algorithm) {
-  for (let i = 10; i <= arrSize; i += 10) {
+  for (let i = 5000; i <= arrSize; i += 5000) {
     let testArr = setupArray(i);
-    // time at start  
-    console.time(`Array Size: ${testArr.length}`);
-    // call method to test
-    testArr.algorithm;
-    // time when finished 
-    console.timeEnd(`Array Size: ${testArr.length}`);
+    shuffle(testArr);
+
+    function run() {
+      testArr.algorithm;
+    }
+
+    const wrapped = performance.timerify(run);
+
+    const obs = new PerformanceObserver((list) => {
+      console.log(`SIZE: ${i}`, list.getEntries()[0].duration, ' ms');
+      obs.disconnect();
+    });
+    obs.observe({ entryTypes: ['function'] });
+
+    // A performance timeline entry will be created
+    wrapped();
   }
 }
 
@@ -22,14 +36,22 @@ function setupArray(arrSize) {
   return arr;
 }
 
+function shuffle(array) {
+  array.sort(() => Math.random() - 0.5);
+}
+
 module.exports = {
   timer
 };
 
 var reverse = Array.prototype.reverse;
 
-timer(1000000, reverse);
+timer(10000000, reverse);
 // setup the array
 // time it with the chosen algorithm
 // grab the results
 // plot the graph
+
+
+
+
